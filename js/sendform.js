d92481f28acb0e75a3ca7dd7dae57877abf6eb0e
ReyzuaWeh <ginanjar.alfarizi07@gmail.com>
2025-12-19 19:57:31 +0700

    Prettier Active

--- js/sendform.js
+++ js/sendform.js
@@ -1,9 +1,10 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbycqPPDXzAGfdaM_r_1yAPFswzVSFfggeo7Oa72PGQ3qn3nmTXv264stupVdsA1K9QrFQ/exec'
-const form = document.forms['submit-to-google-sheet']
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbycqPPDXzAGfdaM_r_1yAPFswzVSFfggeo7Oa72PGQ3qn3nmTXv264stupVdsA1K9QrFQ/exec";
+const form = document.forms["submit-to-google-sheet"];
 
-form.addEventListener('submit', e => {
-  e.preventDefault()
-  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-    .then(response => console.log('Success!', response))
-    .catch(error => console.error('Error!', error.message))
-})
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => console.log("Success!", response))
+    .catch((error) => console.error("Error!", error.message));
+});
